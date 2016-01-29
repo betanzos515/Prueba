06af14de2ffe8908af6b89ea8165352b770ec59b
david  betanzos espinoza <dey_bet@hotmail.com>
2016-01-29 02:06:17 -0600

    Prueba 1

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    //esta es una prueba para ver como funciona github 
 
     override func viewDidLoad() {
         super.viewDidLoad()
